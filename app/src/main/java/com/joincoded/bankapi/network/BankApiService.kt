04d504903fd2f1649279748d69199334850839a0
Amar J M H M H  Alfailakawi <83320@kfh.com>
2024-01-31 09:07:37 +0300

    First

---
+++ app/src/main/java/com/joincoded/bankapi/network/BankApiService.kt
@@ -0,0 +1,43 @@
+package com.joincoded.bankapi.network
+
+import com.joincoded.bankapi.data.AmountChange
+import com.joincoded.bankapi.data.User
+import com.joincoded.bankapi.data.response.TokenResponse
+import com.joincoded.bankapi.utils.Constants
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.PUT
+
+interface BankApiService {
+
+    @POST(Constants.signupEndpoint)
+    suspend fun signup(@Body user: User): Response<TokenResponse>
+
+    @POST(Constants.signinEndpoint)
+    suspend fun signin(@Body user: User): Response<TokenResponse>
+
+    @POST(Constants.transfer)
+    suspend fun transfer(
+        @Body usera: String,
+        password: String,
+        token: String?,
+        amountChange: AmountChange
+    ): Response<TokenResponse>
+
+
+    @PUT(Constants.depositEndpoint)
+    suspend fun deposit(
+        @Header(Constants.authorization) token: String?,
+        @Body amountChange: AmountChange
+    ): Response<Unit>
+
+    @PUT(Constants.withdrawEndpoint)
+    suspend fun withdraw(
+        @Header(Constants.authorization) token: String?,
+        @Body amountChange: AmountChange
+    ): Response<Unit>
+
+
+}
